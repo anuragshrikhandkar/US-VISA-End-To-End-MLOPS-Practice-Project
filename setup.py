57013791f64dc54c514a46a92639daa9caeb0ff2
Anurag Shrikhandkar <anuragshrikhandkar@gmail.com>
2025-07-05 12:25:29 +0530

    repo added sucessfully

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Anurag Shrikhandkar",
+    author_email="anuragshrikhandkar@gmail.com",
+    packages=find_packages()
+)
